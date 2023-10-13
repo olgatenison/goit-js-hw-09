9682b9f80c63dd8a948f182f2b9303f56ebc65d0
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-10-13 13:17:05 +0300

    first done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const elements = {
+  body: document.querySelector('body'),
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+};
+console.log(elements.body);
+console.log(elements.stop);
+console.log(elements.start);
 
+let intervalId;
+
+//generaite random color
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+//add to elem random color
+function addRandomHexColor() {
+  elements.body.style.backgroundColor = getRandomHexColor();
+}
+
+//run the functions every 1000 ms
+elements.start.addEventListener('click', () => {
+  intervalId = setInterval(addRandomHexColor, 1000);
+  elements.start.disabled = true; // Робимо кнопку "Start" неактивною
+  elements.stop.disabled = false; // Робимо кнопку "Stop" активною
+});
+
+//stop generaiting function
+elements.stop.addEventListener('click', () => {
+  clearInterval(intervalId);
+  elements.start.disabled = false; // Робимо кнопку "Start" активною
+  elements.stop.disabled = true; // Робимо кнопку "Stop" неактивною
+});
